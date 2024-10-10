bc48fe1cb79bdb7482b001191f58b52eeb0c0011
skymarshell <144767451+skymarshell@users.noreply.github.com>
2024-10-10 08:19:39 +0000

    first

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+
+const bodyParser = require('body-parser')
+
+
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE",
+  );
+  next();
+});
+
+const products = [
+  {
+    id: 0, name: "Notebook Acer Swift", price: 45900, img: "https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"
+  },
+
+  {
+    id: 1, name: "Notebook Asus  Vivo", price: 19900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"
+  },
+
+  {
+    id: 2, name: "Notebook Lenovo Ideapad", price: 32900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"
+  },
+
+  {
+    id: 3, name: "Notebook MSIPrestige", price: 54900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"
+  },
+
+  {
+    id: 4, name: "Notebook DELLXPS", price: 99900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"
+  },
+
+  {
+    id: 5, name: "Notebook HPEnvy", price: 46900, img: "https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"
+  }];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/products', (req, res) => {
+  if (products.length > 0) {
+    res.send(products)
+  } else {
+    res.status(400).json("No product found.")
+  }
 })
 
 app.listen(port, () => {
