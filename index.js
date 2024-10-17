035a3900fa6729445a81561984b031f3a7236895
skymarshell <144767451+skymarshell@users.noreply.github.com>
2024-10-17 09:11:01 +0000

    insert update delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mysql = require('mysql')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 
@@ -9,7 +10,7 @@ const bodyParser = require('body-parser')
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors())
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -70,6 +71,59 @@ app.get('/api/products/:id', (req, res) => {
   })
 })
 
+app.post('/api/addproduct', (req, res) => {
+  const name = req.body.name
+  const price = req.body.price
+  const img = req.body.img
+  const sql = `INSERT INTO products (name,price,img) VALUES ('${name}','${price}','${img}')`
+  con.query(sql, (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    };
+    con.query("SELECT * from products", (err, result) => {
+      if (err) {
+        console.log(err)
+      }
+      res.send(result)
+    })
+
+  })
+}
+)
+
+app.delete('/api/products/:id', (req, res) => {
+  const id = req.params.id
+  con.query(`DELETE FROM products WHERE id = ${id}`, (err, result) => {
+    if (err) throw err;
+    con.query("SELECT * from products", (err, result) => {
+      if (err) {
+        console.log(err)
+      }
+      res.send(result)
+    })
+  })
+})
+
+app.put('/api/updateproduct/:id',(req,res)=>{
+  const id =req.params.id;
+  const name=req.body.name;
+  const price=req.body.price;
+  const img=req.body.img;
+  console.log(id,name,price,img)
+  con.query(`UPDATE products SET name='${name}',price='${price}',img='${img}' WHERE id=${id}`,function(err,result,fields){
+    if(err) throw res.status(400).send("Error, cannot update product");
+    con.query("SELECT * FROM products",function(err,result,fields){
+      if(err) throw res.status(400).send("No products found");
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    });
+    });
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
